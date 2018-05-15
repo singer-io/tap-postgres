4111e4cde15f63eb1417fcb7cf923b47b09731a8
Sean Byrne <sbyrne@payoff.com>
2018-05-15 13:55:49 -0700

    add dbs_to_discover config option to target specific dbs in discover

--- tap_postgres/__init__.py
+++ tap_postgres/__init__.py
@@ -262,21 +262,26 @@ def discover_db(connection):
 
 def do_discovery(conn_config):
     all_streams = []
-    all_dbs = []
-    with post_db.open_connection(conn_config) as conn:
-        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
-            LOGGER.info("Fetching db's from cluster")
-            cur.execute("""
-            SELECT datname
-              FROM pg_database
-              WHERE datistemplate = false
-                AND CASE WHEN version() LIKE '%Redshift%' THEN true
-                         ELSE has_database_privilege(datname,'CONNECT')
-                    END = true """)
-            all_dbs = cur.fetchall()
+    dbs_to_discover = []
 
-    for db_row in all_dbs:
-        dbname = db_row[0]
+    if conn_config.get('dbs_to_discover'):
+        dbs_to_discover = conn_config['dbs_to_discover']
+    else:
+        with post_db.open_connection(conn_config) as conn:
+            with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+                LOGGER.info("Fetching all db's, to specify a single db include dbs_to_discover in config.json")
+
+                cur.execute("""
+                SELECT datname
+                FROM pg_database
+                WHERE datistemplate = false
+                    AND CASE WHEN version() LIKE '%Redshift%' THEN true
+                            ELSE has_database_privilege(datname,'CONNECT')
+                        END = true """)
+                dbs_to_discover = (row[0] for row in cur.fetchall())
+
+    for db_row in dbs_to_discover:
+        dbname = db_row
         LOGGER.info("Discovering db %s", dbname)
         conn_config['dbname'] = dbname
         with post_db.open_connection(conn_config) as conn:
@@ -450,7 +455,8 @@ def main_impl():
                    'user'     : args.config['user'],
                    'password' : args.config['password'],
                    'port'     : args.config['port'],
-                   'dbname'   : args.config['dbname']}
+                   'dbname'   : args.config['dbname'],
+                   'dbs_to_discover' : args.config.get('dbs_to_discover')}
 
     if args.discover:
         do_discovery(conn_config)
