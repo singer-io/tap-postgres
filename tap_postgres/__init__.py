5979d598990da936bb1d3ad48b1fc33483fb2149
Paul Santa Clara <paul@stitchdata.com>
2018-07-30 17:09:45 -0400

    support materialized views

--- tap_postgres/__init__.py
+++ tap_postgres/__init__.py
@@ -228,7 +228,7 @@ LEFT OUTER JOIN pg_type AS subpgt
  AND pgt.typelem != 0
 WHERE attnum > 0
 AND NOT a.attisdropped
-AND pg_class.relkind IN ('r', 'v')
+AND pg_class.relkind IN ('r', 'v', 'm')
 AND n.nspname NOT in ('pg_toast', 'pg_catalog', 'information_schema')
 AND has_table_privilege(pg_class.oid, 'SELECT') = true """)
         for row in cur.fetchall():
