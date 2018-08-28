c6dec4022ce18a33509d61fc94dca531aab1ad64
Paul Santa Clara <paul@stitchdata.com>
2018-08-28 14:24:26 -0400

    bumping version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-postgres',
-      version='0.0.29',
+      version='0.0.30',
       description='Singer.io tap for extracting data from Oracle',
       author='Stitch',
       url='https://singer.io',
