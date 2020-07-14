5c932412af518ae6d131946717ba60268a9ffd8e
Adam Roderick <aroder@gmail.com>
2020-07-14 14:44:12 -0600

    different import

--- tap_postgres/typecasters/__init__.py
+++ tap_postgres/typecasters/__init__.py
@@ -1,4 +1,4 @@
-import tap_postgres.typecasters.invalid_timestamp_caster as invalid_timestamp_caster
+from . import invalid_timestamp_caster
 
 def register_type_casters(connection):
     invalid_timestamp_caster.register_type(connection)
