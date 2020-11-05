648154bd6b8bcd9a2cf0b77cc61a02d3bf5c70d0
Yordan Ivanov <mail@ivanovyordan.com>
2020-11-05 13:05:13 +0200

    Flexible slot name
    Add a configuration which allows us to set the name of the replication
    slot.

--- tap_postgres/__init__.py
+++ tap_postgres/__init__.py
@@ -684,7 +684,8 @@ def main_impl():
                    'filter_dbs' : args.config.get('filter_dbs'),
                    'debug_lsn' : args.config.get('debug_lsn') == 'true',
                    'logical_poll_total_seconds': float(args.config.get('logical_poll_total_seconds', 0)),
-                   'wal2json_message_format': args.config.get('wal2json_message_format')}
+                   'wal2json_message_format': args.config.get('wal2json_message_format'),
+                   'wal2json_slot_name': args.config.get('wal2json_slot_name')}
 
     if args.config.get('ssl') == 'true':
         conn_config['sslmode'] = 'require'
