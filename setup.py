0dfba0dfc1d400b444698d325f5b875a71bcbc6f
Drew Ipson <drewipson@gmail.com>
2021-11-22 18:56:12 -0700

    Added version to install_requires simplejson

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(name='tap-postgres',
           'singer-python==5.3.1',
           'psycopg2==2.8.6',
           'strict-rfc3339==0.7',
-          'simplejson'
+          'simplejson==3.17.6'
       ],
       extras_require={
           'dev': [
